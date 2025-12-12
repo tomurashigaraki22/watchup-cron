69634eff84059549fc05bddd1baa24605c9014d5
Tomiwa Raphael <emmanuelhudson355@gmail.com>
2025-12-12 08:44:24 +0000

    push

--- db.js
+++ db.js
@@ -12,6 +12,9 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export async function query(sql, params = []) {
