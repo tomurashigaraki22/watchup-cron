eb79a9b5995a1fdf41fe1935b66a38724538f0ec
Tomiwa Raphael <emmanuelhudson355@gmail.com>
2025-12-12 08:36:27 +0000

    fix

--- db.js
+++ db.js
@@ -4,11 +4,11 @@ import mysql from "mysql2/promise";
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: "localhost",
   port: Number(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  user: "watchup",
+  password: "Pityboy@22",
+  database: "watchupweb",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
